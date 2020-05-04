fbcee0d05c1bba863fa4b02b0eeb6fdb7ba4397f
ycazeau <ycazeau23@gmail.com>
2020-05-04 16:48:53 -0400

    Create ForgotPassword, RegisterUser, RegisterSeller Activities / Styling ForgotPassword / Adding action to Button on LoginActivity

--- app/src/main/java/com/example/grocery/LoginActivity.java
+++ app/src/main/java/com/example/grocery/LoginActivity.java
@@ -2,13 +2,47 @@ package com.example.grocery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // User Interface Views
+    private EditText emailEt, passwordEt;
+    private TextView forgotTv, noAccountTv;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+        // Init User Interface Views
+        emailEt = findViewById(R.id.emailEt);
+        passwordEt = findViewById(R.id.passwordEt);
+        forgotTv = findViewById(R.id.forgotTv);
+        noAccountTv = findViewById(R.id.noAccountTv);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        noAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterUserActivity.class));
+            }
+        }); // noAccountTv
+
+        forgotTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
+            }
+        }); // forgotTv
+
+
+    }// End of OnCreate method
+
+} // End of the class
